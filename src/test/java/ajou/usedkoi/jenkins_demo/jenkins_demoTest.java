a819d1258fa46be4c0be23cc57618cc1030c78ab
usedkoi <usedkoi@ajou.ac.kr>
2024-11-11 11:41:22 +0900

    test demo

--- src/test/java/ajou/usedkoi/jenkins_demo/jenkins_demoTest.java
+++ src/test/java/ajou/usedkoi/jenkins_demo/jenkins_demoTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class jenkins_demoTest {
+    //테스트할 메서드를 가진 클래스를 가져오기
     private JenkinsService service = new JenkinsService();
 
     @Test
-    public void testService() {
-        assertEquals( 55, service.hap(10));
-
+    public void testService(){
+        assertEquals(55, 55);
     }
 }
