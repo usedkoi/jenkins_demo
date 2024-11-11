b90a8febb70696055215b3b5ce6bc556ce8c2aed
usedkoi <usedkoi@ajou.ac.kr>
2024-11-11 09:43:56 +0900

    init

---
+++ src/main/java/ajou/usedkoi/jenkins_demo/JenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package ajou.usedkoi.jenkins_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
