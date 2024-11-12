447159b9860775cdc4697706c995265ec6595e8c
usedkoi <usedkoi@ajou.ac.kr>
2024-11-12 14:25:12 +0900

    acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/bash
-test "$(curl -s 'http://localhost:8081?a=1&b=2')" -eq 2
-if [ $? -eq 0 ]; then
-    echo "Test passed"
-else
-    echo "Test failed"
-    exit 1
-fi
+test $(curl http://localhost:8081?a=1\&b=2) -eq 3
